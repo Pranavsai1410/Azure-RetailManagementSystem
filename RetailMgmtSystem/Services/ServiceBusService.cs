7d6615cffab74475e78140cb4714c354015c6b65
Pranavsai Padala <padalapranavsai@gmail.com>
2026-05-15 15:11:01 +0530

    Add files via upload

---
+++ RetailMgmtSystem/Services/ServiceBusService.cs
@@ -0,0 +1,46 @@
+﻿using Azure.Messaging.ServiceBus;
+using Azure.Security.KeyVault.Secrets;
+using RetailInventoryManagement.Services;
+
+public class ServiceBusService
+{
+	private readonly ServiceBusSender _sender;
+	public ServiceBusService(IConfiguration config)
+	{
+		var keyVaultService = new KeyVaultService();
+		var connectionString = keyVaultService.GetSecret("ServiceBusConnection");
+		var queueName = keyVaultService.GetSecret("ServiceBusQueueName");
+
+		var options = new ServiceBusClientOptions
+		{
+			TransportType = ServiceBusTransportType.AmqpWebSockets 
+		};
+
+		var client = new ServiceBusClient(connectionString, options);
+
+		_sender = client.CreateSender(queueName);
+	}
+
+	public async Task SendAlertAsync(string message)
+	{
+		var serviceBusMessage = new ServiceBusMessage(message);
+		await _sender.SendMessageAsync(serviceBusMessage);
+	}
+
+	public async Task SendBatchAlertsAsync(List<string> messages)
+	{
+		ServiceBusMessageBatch batch = await _sender.CreateMessageBatchAsync();
+
+		foreach (var message in messages)
+		{
+			if (!batch.TryAddMessage(new ServiceBusMessage(message)))
+			{
+				await _sender.SendMessagesAsync(batch);
+				batch = await _sender.CreateMessageBatchAsync();
+				batch.TryAddMessage(new ServiceBusMessage(message));
+			}
+		}
+
+		await _sender.SendMessagesAsync(batch);
+	}
+}
---
+++ RetailMgmtSystem/Services/TableStorageService.cs
@@ -0,0 +1,67 @@
+﻿using Azure.Data.Tables;
+
+namespace RetailInventoryManagement.Services
+{
+	public class TableStorageService
+	{
+		private readonly string _connectionString;
+		private readonly string s;
+
+		public TableStorageService(IConfiguration config)
+		{
+			var keyVaultService = new KeyVaultService();
+
+			_connectionString = keyVaultService.GetSecret("TableStorageConnection");
+		}
+
+		// Get Table (creates if not exists)
+		private TableClient GetTable(string tableName)
+		{
+			var client = new TableClient(_connectionString, tableName);
+			client.CreateIfNotExists();
+			return client;
+		}
+
+		// Add Entity
+		public async Task AddEntityAsync<T>(string tableName, T entity)
+			where T : ITableEntity
+		{
+			var table = GetTable(tableName);
+			await table.AddEntityAsync(entity);
+		}
+
+		// Get Single Entity
+		public async Task<T> GetEntityAsync<T>(
+			string tableName, string partitionKey, string rowKey)
+			where T : class, ITableEntity, new()
+		{
+			var table = GetTable(tableName);
+			var result = await table.GetEntityAsync<T>(partitionKey, rowKey);
+			return result.Value;
+		}
+
+		// Get All Entities by PartitionKey
+		public List<T> GetAllEntities<T>(string tableName, string partitionKey)
+			where T : class, ITableEntity, new()
+		{
+			var table = GetTable(tableName);
+			return table.Query<T>(x => x.PartitionKey == partitionKey).ToList();
+		}
+
+		// Update Entity
+		public async Task UpdateEntityAsync<T>(string tableName, T entity)
+			where T : ITableEntity
+		{
+			var table = GetTable(tableName);
+			await table.UpdateEntityAsync(entity, Azure.ETag.All);
+		}
+
+		// Delete Entity
+		public async Task DeleteEntityAsync(
+			string tableName, string partitionKey, string rowKey)
+		{
+			var table = GetTable(tableName);
+			await table.DeleteEntityAsync(partitionKey, rowKey);
+		}
+	}
+}
